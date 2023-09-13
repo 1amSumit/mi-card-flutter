ef2e70be41086aec40fbfaa5990cfce599485cb7
1amSumit <sj779619@gmail.com>
2023-09-13 19:32:29 +0530

    Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Material());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.white,
+                child: Text("hallo from sumit"),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+                child: Text("Hallo form vallo"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
