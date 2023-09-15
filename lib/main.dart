293b43a66c9637c3840e0b19d9c7d27ae8df0fc7
1amSumit <sj779619@gmail.com>
2023-09-15 22:35:30 +0530

    content added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,18 +15,69 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: Text("hallo from sumit"),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avatar.jpg'),
+              ),
+              Text(
+                'Simran Bhanushali',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans 3',
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3.0,
+                    wordSpacing: 3.0),
               ),
               Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-                child: Text("Hallo form vallo"),
-              )
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "+917865432108",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans pro',
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "gmail@gmail.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
